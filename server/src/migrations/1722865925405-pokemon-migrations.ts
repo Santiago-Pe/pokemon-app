379f0726000a827d3bc537bd594041a89d991c06
Santiago-pe <penasantiago346@gmail.com>
2024-08-05 17:51:03 -0300

    create table

--- server/src/migrations/1722865925405-pokemon-migrations.ts
+++ server/src/migrations/1722865925405-pokemon-migrations.ts
@@ -1,11 +1,50 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
 export class PokemonMigrations1722865925405 implements MigrationInterface {
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(new Table({
+      name: 'pokemon',
+      columns: [
+        {
+          name: 'id',
+          type: 'int',
+          isPrimary: true,
+          isGenerated: true,
+        },
+        {
+          name: 'name',
+          type: 'varchar',
+        },
+        {
+          name: 'attack',
+          type: 'int',
+        },
+        {
+          name: 'defense',
+          type: 'int',
+        },
+        {
+          name: 'hp',
+          type: 'int',
+        },
+        {
+          name: 'speed',
+          type: 'int',
+        },
+        {
+          name: 'type',
+          type: 'varchar',
+        },
+        {
+          name: 'imageUrl',
+          type: 'varchar',
+        },
+      ],
+    }));
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('pokemon');
+  }
 }
