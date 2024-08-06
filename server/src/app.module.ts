a43d823a0260f5296040b40b75a025f1728e4bda
Santiago-pe <penasantiago346@gmail.com>
2024-08-06 11:52:22 -0300

    build again backend proyecto

--- server/src/app.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { PokemonModule } from './pokemon/pokemon.module';
-
-@Module({
-  imports: [TypeOrmModule.forRoot(), PokemonModule],
-})
-export class AppModule {}
