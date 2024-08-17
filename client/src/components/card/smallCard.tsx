e594a56ea5e9efc48fc2bf631d848d05237a04e0
Santiago-pe <penasantiago346@gmail.com>
2024-08-17 15:39:40 -0300

    merge wiht local changes}

--- client/src/components/card/smallCard.tsx
+++ client/src/components/card/smallCard.tsx
@@ -13,6 +13,7 @@ const SmallCard: React.FC<CardProps> = ({
   isDisabled = false,
 }) => {
   return (
+<<<<<<< Updated upstream
     <div
       onClick={isDisabled ? undefined : onClick}
       className={`${styles.cardContainer} ${
@@ -26,6 +27,17 @@ const SmallCard: React.FC<CardProps> = ({
         <img src={imageUrl} alt={name} className={styles.cardImage} />
         <CardContent className={styles.cardContent}>
           <Typography className={styles.cardTitle} gutterBottom>
+=======
+    <Box sx={{ maxWidth: 150 }}>
+      <Card>
+        <img
+          src={imageUrl}
+          alt={name}
+          style={{ width: "100%", height: "200px" }}
+        />
+        <CardContent>
+          <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+>>>>>>> Stashed changes
             {name}
           </Typography>
         </CardContent>
--- client/src/pages/home/homePage.tsx
+++ client/src/pages/home/homePage.tsx
@@ -1,50 +1,39 @@
 import React, { useEffect, useState } from "react";
+import { Pokemon } from "../../interfaces/global.interfaces";
 import { useApi } from "../../context/apiContext";
-import { Link, Typography } from "@mui/material";
-import { Battle, PokemonList } from "../../sections";
-import Loading from "../../components/loader/loading/loading";
+import { SmallCard } from "../../components";
+
 import styles from "./homePage.module.css";
-import { useBattleStore } from "../../store/battle/battle.store";
 
 const Home: React.FC = () => {
-  const { getPokemons } = useApi();
-  const { setAllPokemons, allPokemons } = useBattleStore();
-
-  const [loading, setLoading] = useState<boolean>(true);
+  const { getPokemons, getBattles } = useApi();
+  const [pokemons, setPokemons] = useState<Pokemon[]>([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const fetchedPokemons = await getPokemons();
-        setAllPokemons(fetchedPokemons);
+        const allPokemons = await getPokemons();
+        setPokemons(allPokemons);
       } catch (error) {
         console.error("Error fetching data:", error);
-      } finally {
-        setLoading(false);
       }
     };
 
     fetchData();
-  }, [getPokemons, setAllPokemons]);
-
-  if (loading) {
-    return <Loading />;
-  }
+  }, [getPokemons, getBattles]);
 
   return (
-    <section className={styles.container}>
-      <Typography variant="h4" component="h1" className={styles.title}>
-        Pokémon Battles
-      </Typography>
-      <Link href="/battle-log" underline="none">
-        Logs
-      </Link>
-      <Typography variant="h5" component="h2" className={styles.subtitle}>
-        Select your pokemon
-      </Typography>
-
-      <PokemonList />
-      {allPokemons.length > 0 && <Battle />}
+    <section>
+      <h1>Pokémon Battles</h1>
+      <ul className={styles.containerList}>
+        {pokemons.map((pokemon) => (
+          <SmallCard
+            imageUrl={pokemon.imageUrl}
+            name={pokemon.name}
+            key={pokemon.id}
+          />
+        ))}
+      </ul>
     </section>
   );
 };
